4b9814a2bebf6065ab6e471fbfa9e0cf74c78576
David Pérez Espino <dpereze1105@gmail.com>
2016-02-25 17:41:46 -0600

    otro avance de editar empleados

--- resources/views/pt_dash/editar_empleados.blade.php
+++ resources/views/pt_dash/editar_empleados.blade.php
@@ -8,10 +8,8 @@
       <div class="row">
         {{ $editable->id }}<br>
 
-        {{ $editable->fecha_nac }}<br>
-        {{ $editable->sexo }}<br>
-        {{ $editable->foto }}<br>
-        <img src="<?php //echo 'data:image/jpeg;base64,'.base64_encode( $editable->foto )?>" class="user-image" alt="User Image">
+        <br>
+        
         {{ $editable->direccion }}<br>
         {{ $editable->telefono }}<br>
         {{ $editable->hora_init }}<br>
@@ -31,24 +29,89 @@
 
                   <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                    <input type="text" name="nombre" class="form-control" placeholder="{{ $editable->nombre }}">
+                    <input type="text" name="nombre" class="form-control" value="{{ $editable->nombre }}">
                   </div>
 
                   <br>
 
                   <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                    <input type="text" name="ap_pat" class="form-control" placeholder="{{ $editable->ap_pat }}">
+                    <input type="text" name="ap_pat" class="form-control" value="{{ $editable->ap_pat }}">
                   </div>
 
                   <br>
 
                   <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                    <input type="text" name="ap_mat" class="form-control" placeholder="{{ $editable->ap_mat }}">
+                    <input type="text" name="ap_mat" class="form-control" value="{{ $editable->ap_mat }}">
                   </div>
 
                 </div><!-- /.box-body -->
+
+
+
+              </div><!-- /.box -->
+
+
+
+            </div><!--/.col (left) -->
+
+            <!-- right column -->
+            <div class="col-md-6">
+              <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Información personal</h3>
+                </div><!-- /.box-header -->
+
+                <div class="box-body">
+
+                  <div class="form-group">
+                    <label>Fecha de Nacimiento</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+                      <input type="date" class="form-control" value="{{ $editable->fecha_nac }}">
+                    </div>
+                  </div>
+
+                  <br>
+
+                  <div class="form-group">
+                    <label> Sexo </label><br>
+                    
+                    @if($editable->sexo == 1)
+                      <label>
+                        <input type="radio" name="sexo" class="flat-blue" checked>
+                        Masculino
+                      </label><br>
+                      <label>
+                        <input type="radio" name="sexo" class="flat-blue">
+                        Femenino
+                      </label>
+                    @else
+                      <label>
+                        <input type="radio" name="sexo" class="flat-blue">
+                        Masculino
+                      </label><br>
+                      <label>
+                        <input type="radio" name="sexo" class="flat-blue" checked>
+                        Femenino
+                      </label>
+                    @endif
+
+                  </div>
+                  
+                  <br>
+
+                  <div class="form-group" ><br>
+                    <label for="exampleInputFile">Foto</label><br>
+                    <img src="<?php echo 'data:image/jpeg;base64,'.base64_encode( $editable->foto )?>" alt="Foto" style="height: 100px; width: 100px; border-radius: 50%; margin: 0 auto;"> 
+                    <br><br>
+                    <input type="file" id="exampleInputFile">
+                    <p class="help-block">Example block-level help text here.</p>
+                  </div>
+                </div>
 
 
                 <div class="box-footer">
@@ -56,13 +119,6 @@
                 </div>
 
               </div><!-- /.box -->
-
-
-
-            </div><!--/.col (left) -->
-            <!-- right column -->
-            <div class="col-md-6">
-
 
             </div><!--/.col (right) -->
 
@@ -78,4 +134,18 @@
 
 <script src="/plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="/plugins/datatables/dataTables.bootstrap.min.js"></script>
+
+<!-- iCheck 1.0.1 -->
+<script src="/plugins/iCheck/icheck.min.js"></script>
+
+<script >
+  $(function() {
+    //Flat red color scheme for iCheck
+      $('input[type="checkbox"].flat-red, input[type="radio"].flat-blue').iCheck({
+        checkboxClass: 'icheckbox_flat-blue',
+        radioClass: 'iradio_flat-blue'
+      });
+  });
+</script>
+
 @endsection
