5d28f4ce189d83bc0672bf11ac33dd72e19c3f3a
David Perez Espino <david.perez@mercadodecredito.com>
2016-03-01 22:15:53 -0600

    Home con fullpage

--- resources/views/pt_page/home.blade.php
+++ resources/views/pt_page/home.blade.php
@@ -13,8 +13,8 @@
 
 	<link rel="stylesheet" type="text/css" href="/plugins/fullPage/jquery.fullPage.css" />
 	
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+	<script src="/plugins/jQuery/jQuery-2.2.0.min.js"></script>
+	<script src="/plugins/jQueryUI/jQquery-ui.min.js"></script>
 
 	<script type="text/javascript" src="/plugins/fullPage/jquery.fullPage.min.js"></script>
 	
@@ -42,35 +42,13 @@
 		<img src="imgs/fullPage.png" alt="fullPage" />
 	</div>
 	<div class="section" id="section1">
-	    <div class="slide">
-			<div class="intro">
+	    
+	    	<div class="intro">
 				<h1>Create Sliders</h1>
 				<p>Not only vertical scrolling but also horizontal scrolling. With fullPage.js you will be able to add horizontal sliders in the most simple way ever.</p>
 				<img src="imgs/slider.png" alt="slider" />
 			</div>
 
-		</div>
-	    <div class="slide">
-			<div class="intro">
-				<img src="imgs/1.png" alt="simple" />
-				<h1>Simple</h1>
-				<p>Easy to use. Configurable and customizable.</p>
-			</div>
-		</div>
-	    <div class="slide">
-			<div class="intro">
-				<img src="imgs/2.png" alt="Cool" />
-				<h1>Cool</h1>
-				<p>It just looks cool. Impress everybody with a simple and modern web design!</p>
-			</div>
-		</div>
-	    <div class="slide">
-			<div class="intro">
-				<img src="imgs/3.png" alt="Compatible" />
-				<h1>Compatible</h1>
-				<p>Working in modern and old browsers too! IE 8 users don't have the fault of using that horrible browser! Lets give them a chance to see your site in a proper way!</p>
-			</div>
-		</div>
 	</div>
 	<div class="section" id="section2">
 		<div class="intro">
